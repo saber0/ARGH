9937ef79b223ccae876abaeb01df95222b24dd8b
shirazc <shirazchaudhry1@gmail.com>
2018-08-14 18:27:44 -0400

    Final Commit before code clean-up.

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample/sample.fxml"));
         primaryStage.setTitle("Sorter of Sorts");
-        primaryStage.setScene(new Scene(root, 777, 777));
+        primaryStage.setScene(new Scene(root, 777, 555));
         primaryStage.show();
     }
 
